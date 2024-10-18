6c17627a6c80b9f1f44bfdac535ba462639b5ddb
ashenone1379 <2920130195@qq.com>
2024-10-18 18:08:18 +0800

    完成lab01

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -2,6 +2,16 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* your code here */
+    if (head == NULL)
+        return 0;
+    const node *ptr1 = head;
+    const node *ptr2 = head->next;
+    while (ptr2 != NULL && ptr2->next != NULL) {
+        if (ptr1 == ptr2) {
+            return 1;
+        }
+        ptr1 = ptr1->next;
+        ptr2 = ptr2->next->next;
+    }
     return 0;
 }
